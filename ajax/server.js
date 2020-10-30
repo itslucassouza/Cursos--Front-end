87e86ef8e9c6e986c2033f783953a129e9da6460
Lucas Souza <lucasvini193@hotmail.com>
2020-10-30 11:00:32 -0300

    Atualização de cursos Finalizados

--- ajax/server.js
+++ ajax/server.js
@@ -1,10 +1,11 @@
 const bodyParser= require('body-Parser')
 const express = require('express')
+
 const app = express()
 
-app.use(express.static('.'))
+app.use(express.static('.')) //servido as paginas estaticas 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('ok'))
+// app.get('/teste', (req, res) => res.send('ok')) 
 app.listen(8080, () => console.log('executando..'))
---
+++ nodeJs - COMPLETO/Projeto/src/servidor.js
@@ -0,0 +1,38 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados') 
+
+app.use(bodyParser.urlencoded({ extended: true }))
+//o use atende em qualquer url que coloque 
+// app.use('/produtos', (req, res, next)=> {
+    
+// })
+
+app.get('/produtos', (req, res, next)=> {
+    res.send(bancoDeDados.getProdutos()) 
+})
+
+app.get('/produtos/:id', (req, res, nex) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProdutos(req.params.id)
+    res.send(produto)
+})
+
+
+app.listen(porta, () => {
+    console.log(`Servidor executando na porta ${porta}`)
+})
